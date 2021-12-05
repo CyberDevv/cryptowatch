38268a88308f9b52e60e2d92861ebfaeaf9ee707
CyberDevv <tocyberdevv@gmail.com>
2021-12-05 11:05:50 +0100

    Made the sidebar and also added fonts

--- src/components/Layout/SideBar.jsx
+++ src/components/Layout/SideBar.jsx
@@ -1,10 +1,85 @@
 import tw from 'twin.macro';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import { Logo, HomeSVG, PlaceholderSVG, SVGIcons } from '../SVG-Icons';
 
 const SideBar = () => {
-   return <Nav>Sidebar</Nav>;
+   const [active, setActive] = useState('Home');
+   return (
+      <Nav>
+         {/* Logo */}
+         <Link href='/'>
+            <LogoAnchor>
+               <Logo />
+            </LogoAnchor>
+         </Link>
+
+         {/* NavMenu */}
+         <NavMenu>
+            <NavItemComponent
+               active={active}
+               link='/'
+               svg={<SVGIcons active={active} home />}
+               label='Home'
+            />
+            <NavItemComponent
+               active={active}
+               link='/'
+               svg={<SVGIcons active={active} watchlist />}
+               label='Watchlist'
+            />
+            <NavItemComponent
+               active={active}
+               link='/'
+               svg={<SVGIcons active={active} wallet />}
+               label='Wallet'
+            />
+            <NavItemComponent
+               active={active}
+               link='/'
+               svg={<SVGIcons active={active} priceAlerts />}
+               label='Price Alerts'
+            />
+            <NavItemComponent
+               active={active}
+               link='/'
+               svg={<SVGIcons active={active} settings />}
+               label='Settings'
+            />
+            <NavItemComponent
+               active={active}
+               link='/'
+               svg={<SVGIcons active={active} logout />}
+               label='Log out'
+            />
+         </NavMenu>
+      </Nav>
+   );
+};
+
+const NavItemComponent = ({ active, label, svg, link }) => {
+   return (
+      <NavItem>
+         <Link href={link}>
+            <Anchor
+               css={[
+                  active === label ? tw`text-primary-blue` : tw`text-dark-gray`,
+               ]}
+            >
+               {svg}
+               <p className='small'>{label}</p>
+            </Anchor>
+         </Link>
+      </NavItem>
+   );
 };
 
 // tailwind Styles
-const Nav = tw.nav`lg:(w-[180px] min-w-[180px]) xl:(w-[211px] min-w-[211px])`;
+const Nav = tw.nav`lg:(w-[180px] min-w-[180px] py-12 ml-6) xl:(w-[211px] min-w-[211px])`;
+const LogoAnchor = tw.a`ml-4`;
+const NavMenu = tw.ul`lg:(my-[70px] space-y-10)`;
+const NavItem = tw.li``;
+const Anchor = tw.a`cursor-pointer flex items-center lg:(space-x-4) letter-spacing[-0.025em]`;
 
 export default SideBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,32 @@ module.exports = {
    darkMode: false, // or 'media' or 'class'
    theme: {
       extend: {
-         primary: {
-            softblue: '#C7D3FF',
-            bluee: '#7895FF',
-            default: '#4C6FFF',
-            blue: '#3754DB',
-          gradient: {
-              1: '#D665FF',
-              2: '#4C6FFF',
+         colors: {
+            primary: {
+               softblue: '#C7D3FF',
+               bluee: '#7895FF',
+               default: '#4C6FFF',
+               blue: '#3754DB',
+               gradient: {
+                  1: '#D665FF',
+                  2: '#4C6FFF',
+               },
             },
-         },
-         secondary: {
-           gradient: {
-              1: '#FF92AE',
-              2: '#FF3D9A',
+            secondary: {
+               gradient: {
+                  1: '#FF92AE',
+                  2: '#FF3D9A',
+               },
+               style: '#E52F15',
+               2: '#FF385C',
+               4: '#F6ACA2',
+               3: '#FFE5EA',
             },
-            'style': '#E52F15',
-            2: '#FF385C',
-            4: '#F6ACA2',
-            3: '#FFE5EA',
-         },
-         dark: {
-            gray: '#999999',
-            black: '#666666',
-            darker: '#333333',
+            dark: {
+               gray: '#999999',
+               black: '#666666',
+               darker: '#333333',
+            },
          },
       },
    },
