7cc2d657c89c33f93f0f7eb0e658a4ef4d1c10f9
CyberDevv <tocyberdevv@gmail.com>
2021-12-10 12:47:40 +0100

    Added nprogress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import { GlobalStyles } from 'twin.macro';
+import NextNProgress from 'nextjs-progressbar';
 
 import '../styles/globals.css';
 
@@ -6,6 +7,7 @@ function MyApp({ Component, pageProps }) {
    const user = 'null';
    return (
       <>
+         <NextNProgress height={5} />
          <GlobalStyles />
          <Component user={user} {...pageProps} />
       </>
