b34db5a5457225b25f047995914dba300ce18e5b
CyberDevv <tocyberdevv@gmail.com>
2021-12-14 21:07:13 +0100

    made a tiny fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,11 +20,12 @@ export async function getStaticProps() {
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d';
 
    const res = await (await axios(url)).data;
-   
+
    return {
       props: {
          coins: res,
       },
+      fallback: true,
       revalidate: 30,
    };
 }
